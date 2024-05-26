b3d9bb6fd8677c66d81ac49c15808c41c1652765
Timothy Pfafman <tim@pfafman.com>
2024-05-26 08:19:51 -0600

    working

--- meteorDeploy.sh
+++ meteorDeploy.sh
@@ -1,44 +1,101 @@
 #!/bin/bash
 
+if [ "$1" == "-d" ]; then 
+	echo "Deploy Only, no build"; 
+fi
+
+echo "Reading mup.json file ..."
+config=`cat mup.json | grep -v "^[[:blank:]]*//"`
 echo ""
 
-echo "\tReading mup.json file ..."
-config=`cat mup.json | grep -v "//"`
-
-
 date=$(date '+%Y-%m-%d')
-
 appName=`echo $config | jq -r '.appName'`
-
-
 buildLocaltion="/tmp/mup/$appName/$date"
-
 
 # meteor build /tmp/mup/coWorkr1/2024-05-25 --architecture os.linux.x86_64 --server http://localhost:3000 --server-onl
 buildCmd="meteor build $buildLocaltion --architecture os.linux.x86_64 --server http://localhost:3000 --server-only"
 
 # cd to app
 appDir=`echo $config | jq -r '.app'`
-echo "\tcd $appDir"
-
 currentDir=`pwd`
+scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd $appDir
 
-echo " Running: $buildCmd ..."
-echo ""
+if [ "$1" != "-d" ]; then 
+	echo "cd $appDir"
+	cd $appDir
+	echo ""
+	echo "$buildCmd ..."
+	echo ""
+	$buildCmd
+	cd $currentDir
+	echo ""
+fi
 
-$buildCmd
+servers=`echo $config | jq -r '.servers.[]'`
 
-echo "\tcd $currentDir"
-cd $currentDir
-
-echo ""
-
-servers=`echo $config | jq -r '.servers.[].host'`
 
 for server in "${servers[@]}"
 do
-	echo "\tscp $buildLocaltion/bundle.tar.gz $server:/opt/$appName/tmp/."
-	scp $buildLocaltion/app.tar.gz $server:/opt/$appName/tmp/.
+	host=`echo $server | jq -r '.host'`
+
+	echo "Copy $appName to $host"
+	scp $buildLocaltion/app.tar.gz $host:/opt/$appName/tmp/.
+	echo ""
+
+	env=`echo $server | jq -r '.env'`
+	keys=($(echo $env | jq -r 'keys' | tr -d '[]," '))
+	rm $buildLocaltion/env.sh
+	for key in "${keys[@]}"
+	do
+		val=`echo $env | jq ".$key"`
+		echo "$key: $val" >> $buildLocaltion/env.sh
+	done
+	echo -n "METEOR_SETTINGS=" >> $buildLocaltion/env.sh
+	cat settings.json >> $buildLocaltion/env.sh
+	echo "" >> $buildLocaltion/env.sh
+	echo ""
+	cat $buildLocaltion/env.sh
+	echo "Copy env.sh to $host"
+	scp $buildLocaltion/env.sh $host:/opt/$appName/tmp/.
+	echo ""
+
+	port=`echo $env | jq -r '.PORT'`
+	
+	# Run installer on remote
+	echo "Install on $host $port"
+	echo ""
+
+	remoteCmd=$(cat <<-END
+	    cd /opt/$appName/tmp;
+	    echo "Extracting";
+	    tar xfz app.tar.gz;
+	    cd ..;
+	    sudo rm -rf old_app;
+	    sudo rm -rf old_config;
+	    echo "BackUp Old App";
+	    sudo mv app old_app;
+	    sudo cp -r config old_config;
+	    sudo mv tmp/bundle app;
+	    sudo mv tmp/env.sh config/.;
+	    cd app/programs/server && sudo npm install;
+	    echo "Restarting $appName";
+	    sudo service $appName restart;
+	    sleep 5;
+	    echo "Checking $appName on port:$port";
+	    if curl localhost:$port 2>/dev/null >/dev/null; then
+    		echo "Server online";
+    	else
+    		echo "Server offline";
+		fi
+END
+	)
+
+	ssh $host $remoteCmd
+
 done
+
+if [ "$1" != "-s" ]; then
+	echo "Clean Up"
+fi 
+
